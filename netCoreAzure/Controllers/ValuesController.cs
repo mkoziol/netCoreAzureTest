7ad5852d622023d50b48b71a4c3818264175cfb0
Michał Kozioł <michalkoziol5@windowslive.com>
2017-07-25 10:36:53 +0200

    asd

---
+++ netCoreAzure/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace netCoreAzure.Controllers
+{
+    [Route("api/Sleepy")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            System.Threading.Thread.Sleep(5000);
+            return new string[] { "Fender", "Gibson" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            System.Threading.Thread.Sleep(6000);
+            return $"The value you sent was: {id} ";
+        }
+    }
+}
