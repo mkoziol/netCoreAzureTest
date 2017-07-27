f74d89619543770f6c431484710d9eaf0598355f
Michał Kozioł <michalkoziol5@windowslive.com>
2017-07-27 10:11:33 +0200

    DB

--- netCoreAzure/Controllers/ValuesController.cs
+++ netCoreAzure/Controllers/ValuesController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using netCoreAzure.Data;
 
 namespace netCoreAzure.Controllers
 {
@@ -16,13 +18,22 @@ namespace netCoreAzure.Controllers
             System.Threading.Thread.Sleep(5000);
             return new string[] { "Fender", "Gibsoan" };
         }
-
+        
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public List<tableName> Get(int id)
         {
-            System.Threading.Thread.Sleep(6000);
-            return $"The value you sent was xax123dev6: {id} ";
+            string name = "";
+
+            List<tableName> list= new List<tableName>();
+            using (Context context = new Context())
+            {
+
+                list = context.Users.ToList();
+            }
+
+
+            return list;
         }
     }
 }
---
+++ netCoreAzure/Data/tableName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netCoreAzure.Data
+{
+    public class tableName
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
